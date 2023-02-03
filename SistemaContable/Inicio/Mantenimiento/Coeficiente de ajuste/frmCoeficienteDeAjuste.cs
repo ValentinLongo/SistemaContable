274b7ae5d7bd405de2b8c13d58629752fb56728a
Valentin <Valentin@SERVERMASER>
2023-02-03 09:23:19 -0300

    Vale Prueba 03/02

---
+++ SistemaContable/Inicio/Mantenimiento/Coeficiente de ajuste/frmCoeficienteDeAjuste.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaContable.Inicio.Mantenimiento.Coeficiente_de_ajuste
+{
+    public partial class frmCoeficienteDeAjuste : Form
+    {
+        public frmCoeficienteDeAjuste()
+        {
+            InitializeComponent();
+        }
+    }
+}
