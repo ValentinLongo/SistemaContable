86f772ebe27cc3903daa8d8b8aec22637ddc889e
Valentin <Valentin@SERVERMASER>
2023-05-16 10:54:31 -0300

    Vale 16/05

--- SistemaContable/Inicio/Mantenimiento/Rubricación de SubDiarios/frmRubricacionDeSubDiarios.Designer.cs
+++ SistemaContable/Inicio/Mantenimiento/Rubricación de SubDiarios/frmRubricacionDeSubDiarios.Designer.cs
@@ -53,7 +53,15 @@
             this.panel8 = new System.Windows.Forms.Panel();
             this.ShapeBusqueda = new Bunifu.UI.WinForms.BunifuShapes();
             this.label7 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.impresiónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.guardarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.imprimirPorPantallaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.imprimirPorImpresoraToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.panel7.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel7
@@ -62,7 +70,7 @@
             this.panel7.Controls.Add(this.lblControlBar);
             this.panel7.Controls.Add(this.bunifuFormControlBox1);
             this.panel7.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel7.Location = new System.Drawing.Point(0, 0);
+            this.panel7.Location = new System.Drawing.Point(0, 24);
             this.panel7.Name = "panel7";
             this.panel7.Size = new System.Drawing.Size(775, 21);
             this.panel7.TabIndex = 96;
@@ -148,7 +156,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Franklin Gothic Medium", 9.75F);
             this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label1.Location = new System.Drawing.Point(70, 147);
+            this.label1.Location = new System.Drawing.Point(64, 167);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(78, 17);
             this.label1.TabIndex = 97;
@@ -159,7 +167,7 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Franklin Gothic Medium", 9.75F);
             this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label2.Location = new System.Drawing.Point(70, 191);
+            this.label2.Location = new System.Drawing.Point(64, 211);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(78, 17);
             this.label2.TabIndex = 98;
@@ -170,7 +178,7 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Franklin Gothic Medium", 9.75F);
             this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label3.Location = new System.Drawing.Point(70, 278);
+            this.label3.Location = new System.Drawing.Point(64, 298);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(78, 17);
             this.label3.TabIndex = 99;
@@ -181,7 +189,7 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Franklin Gothic Medium", 9.75F);
             this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label4.Location = new System.Drawing.Point(70, 235);
+            this.label4.Location = new System.Drawing.Point(64, 255);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(78, 17);
             this.label4.TabIndex = 100;
@@ -190,7 +198,7 @@
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.White;
-            this.panel2.Location = new System.Drawing.Point(154, 296);
+            this.panel2.Location = new System.Drawing.Point(148, 316);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(542, 1);
             this.panel2.TabIndex = 118;
@@ -201,7 +209,7 @@
             this.Campo4.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.Campo4.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Campo4.ForeColor = System.Drawing.SystemColors.Window;
-            this.Campo4.Location = new System.Drawing.Point(154, 275);
+            this.Campo4.Location = new System.Drawing.Point(148, 295);
             this.Campo4.Name = "Campo4";
             this.Campo4.Size = new System.Drawing.Size(542, 19);
             this.Campo4.TabIndex = 5;
@@ -213,7 +221,7 @@
             this.Campo3.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.Campo3.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Campo3.ForeColor = System.Drawing.SystemColors.Window;
-            this.Campo3.Location = new System.Drawing.Point(154, 232);
+            this.Campo3.Location = new System.Drawing.Point(148, 252);
             this.Campo3.Name = "Campo3";
             this.Campo3.Size = new System.Drawing.Size(542, 19);
             this.Campo3.TabIndex = 4;
@@ -222,7 +230,7 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.White;
-            this.panel1.Location = new System.Drawing.Point(154, 253);
+            this.panel1.Location = new System.Drawing.Point(148, 273);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(542, 1);
             this.panel1.TabIndex = 120;
@@ -233,7 +241,7 @@
             this.Campo2.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.Campo2.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Campo2.ForeColor = System.Drawing.SystemColors.Window;
-            this.Campo2.Location = new System.Drawing.Point(154, 188);
+            this.Campo2.Location = new System.Drawing.Point(148, 208);
             this.Campo2.Name = "Campo2";
             this.Campo2.Size = new System.Drawing.Size(542, 19);
             this.Campo2.TabIndex = 3;
@@ -242,7 +250,7 @@
             // panel3
             // 
             this.panel3.BackColor = System.Drawing.Color.White;
-            this.panel3.Location = new System.Drawing.Point(154, 209);
+            this.panel3.Location = new System.Drawing.Point(148, 229);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(542, 1);
             this.panel3.TabIndex = 122;
@@ -253,7 +261,7 @@
             this.Campo1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.Campo1.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Campo1.ForeColor = System.Drawing.SystemColors.Window;
-            this.Campo1.Location = new System.Drawing.Point(154, 145);
+            this.Campo1.Location = new System.Drawing.Point(148, 165);
             this.Campo1.Name = "Campo1";
             this.Campo1.Size = new System.Drawing.Size(542, 19);
             this.Campo1.TabIndex = 2;
@@ -262,7 +270,7 @@
             // panel4
             // 
             this.panel4.BackColor = System.Drawing.Color.White;
-            this.panel4.Location = new System.Drawing.Point(154, 166);
+            this.panel4.Location = new System.Drawing.Point(148, 186);
             this.panel4.Name = "panel4";
             this.panel4.Size = new System.Drawing.Size(542, 1);
             this.panel4.TabIndex = 124;
@@ -272,7 +280,7 @@
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Franklin Gothic Medium", 9.75F);
             this.label5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label5.Location = new System.Drawing.Point(34, 53);
+            this.label5.Location = new System.Drawing.Point(28, 73);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(114, 17);
             this.label5.TabIndex = 126;
@@ -283,7 +291,7 @@
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Franklin Gothic Medium", 9.75F);
             this.label6.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label6.Location = new System.Drawing.Point(46, 86);
+            this.label6.Location = new System.Drawing.Point(40, 106);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(102, 17);
             this.label6.TabIndex = 127;
@@ -292,7 +300,7 @@
             // panel5
             // 
             this.panel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(52)))), ((int)(((byte)(162)))));
-            this.panel5.Location = new System.Drawing.Point(13, 134);
+            this.panel5.Location = new System.Drawing.Point(7, 154);
             this.panel5.Name = "panel5";
             this.panel5.Size = new System.Drawing.Size(750, 1);
             this.panel5.TabIndex = 132;
@@ -303,7 +311,7 @@
             this.FolioHasta.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.FolioHasta.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FolioHasta.ForeColor = System.Drawing.SystemColors.Window;
-            this.FolioHasta.Location = new System.Drawing.Point(154, 83);
+            this.FolioHasta.Location = new System.Drawing.Point(148, 103);
             this.FolioHasta.Name = "FolioHasta";
             this.FolioHasta.Size = new System.Drawing.Size(113, 19);
             this.FolioHasta.TabIndex = 1;
@@ -312,7 +320,7 @@
             // panel6
             // 
             this.panel6.BackColor = System.Drawing.Color.White;
-            this.panel6.Location = new System.Drawing.Point(154, 104);
+            this.panel6.Location = new System.Drawing.Point(148, 124);
             this.panel6.Name = "panel6";
             this.panel6.Size = new System.Drawing.Size(113, 1);
             this.panel6.TabIndex = 133;
@@ -323,7 +331,7 @@
             this.FolioDesde.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.FolioDesde.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FolioDesde.ForeColor = System.Drawing.SystemColors.Window;
-            this.FolioDesde.Location = new System.Drawing.Point(154, 49);
+            this.FolioDesde.Location = new System.Drawing.Point(148, 69);
             this.FolioDesde.Name = "FolioDesde";
             this.FolioDesde.Size = new System.Drawing.Size(113, 19);
             this.FolioDesde.TabIndex = 0;
@@ -332,7 +340,7 @@
             // panel8
             // 
             this.panel8.BackColor = System.Drawing.Color.White;
-            this.panel8.Location = new System.Drawing.Point(154, 70);
+            this.panel8.Location = new System.Drawing.Point(148, 90);
             this.panel8.Name = "panel8";
             this.panel8.Size = new System.Drawing.Size(113, 1);
             this.panel8.TabIndex = 135;
@@ -346,7 +354,7 @@
             this.ShapeBusqueda.BorderThickness = 1;
             this.ShapeBusqueda.FillColor = System.Drawing.Color.Transparent;
             this.ShapeBusqueda.FillShape = true;
-            this.ShapeBusqueda.Location = new System.Drawing.Point(12, 37);
+            this.ShapeBusqueda.Location = new System.Drawing.Point(6, 64);
             this.ShapeBusqueda.Name = "ShapeBusqueda";
             this.ShapeBusqueda.Shape = Bunifu.UI.WinForms.BunifuShapes.Shapes.Rectangle;
             this.ShapeBusqueda.Sides = 5;
@@ -359,18 +367,74 @@
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Franklin Gothic Medium", 9.75F);
             this.label7.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label7.Location = new System.Drawing.Point(24, 27);
+            this.label7.Location = new System.Drawing.Point(18, 47);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(54, 17);
             this.label7.TabIndex = 138;
             this.label7.Text = "Imprimir";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem,
+            this.impresiónToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(775, 24);
+            this.menuStrip1.TabIndex = 139;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abrirToolStripMenuItem,
+            this.guardarToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // impresiónToolStripMenuItem
+            // 
+            this.impresiónToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.imprimirPorPantallaToolStripMenuItem,
+            this.imprimirPorImpresoraToolStripMenuItem});
+            this.impresiónToolStripMenuItem.Name = "impresiónToolStripMenuItem";
+            this.impresiónToolStripMenuItem.Size = new System.Drawing.Size(72, 20);
+            this.impresiónToolStripMenuItem.Text = "Impresión";
+            // 
+            // abrirToolStripMenuItem
+            // 
+            this.abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+            this.abrirToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.abrirToolStripMenuItem.Text = "Abrir";
+            this.abrirToolStripMenuItem.Click += new System.EventHandler(this.abrirToolStripMenuItem_Click);
+            // 
+            // guardarToolStripMenuItem
+            // 
+            this.guardarToolStripMenuItem.Name = "guardarToolStripMenuItem";
+            this.guardarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.guardarToolStripMenuItem.Text = "Guardar";
+            this.guardarToolStripMenuItem.Click += new System.EventHandler(this.guardarToolStripMenuItem_Click_1);
+            // 
+            // imprimirPorPantallaToolStripMenuItem
+            // 
+            this.imprimirPorPantallaToolStripMenuItem.Name = "imprimirPorPantallaToolStripMenuItem";
+            this.imprimirPorPantallaToolStripMenuItem.Size = new System.Drawing.Size(197, 22);
+            this.imprimirPorPantallaToolStripMenuItem.Text = "Imprimir por Pantalla";
+            this.imprimirPorPantallaToolStripMenuItem.Click += new System.EventHandler(this.imprimirPorPantallaToolStripMenuItem_Click);
+            // 
+            // imprimirPorImpresoraToolStripMenuItem
+            // 
+            this.imprimirPorImpresoraToolStripMenuItem.Name = "imprimirPorImpresoraToolStripMenuItem";
+            this.imprimirPorImpresoraToolStripMenuItem.Size = new System.Drawing.Size(197, 22);
+            this.imprimirPorImpresoraToolStripMenuItem.Text = "Imprimir por Impresora";
             // 
             // frmRubricacionDeSubDiarios
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
-            this.ClientSize = new System.Drawing.Size(775, 313);
+            this.ClientSize = new System.Drawing.Size(775, 327);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.FolioDesde);
             this.Controls.Add(this.panel8);
@@ -393,12 +457,16 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.panel7);
             this.Controls.Add(this.ShapeBusqueda);
+            this.Controls.Add(this.menuStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "frmRubricacionDeSubDiarios";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "frmRubricacionDeSubDiarios";
             this.panel7.ResumeLayout(false);
             this.panel7.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -430,5 +498,12 @@
         private System.Windows.Forms.Panel panel8;
         private Bunifu.UI.WinForms.BunifuShapes ShapeBusqueda;
         private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abrirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem guardarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem impresiónToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem imprimirPorPantallaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem imprimirPorImpresoraToolStripMenuItem;
     }
 }
